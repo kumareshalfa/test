f916590282ed9e2c9ee594da261e0cbc5ba4fd6b
sakthivel-freelancer <sakthivelramanp@gmail.com>
2018-12-07 23:37:24 +0530

    new changes

--- news.py
+++ news.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import scrapy
 import json
 from scrapy.http import FormRequest
@@ -12,13 +13,37 @@ class BlogSpider(scrapy.Spider):
 		super(BlogSpider, self).__init__(*args, **kwargs)
 		self.start_urls = ['http://www.alazraq.com']
 	def parse(self, response):
-		i=0
-		for news in response.css('a'):
+		for news in response.css('.category'):
 			next_page = news.css('a::attr("href")').extract_first()
-			print("-----------------------------------")
+			if next_page:
+				yield scrapy.Request(response.urljoin(next_page),callback=self.get_pages)
+	def get_pages(self,response):
+		href = response.url
+		page = response.css('td:nth-child(3) font.Category ::text').extract_first()
+		page = page.replace(u'من','')
+		page = page.replace(u'صفحة ','')
+		page = page.replace(u'\u0627\u0644\n\xa01\xa0\n\n\xa0','')
+		for i in range(1,(int(page)+1)):
+			page_href = href+"&screen="+str(i)
+			yield scrapy.Request(response.urljoin(page_href),callback=self.category_list)
+	def category_list(self,response):
+		href = response.url
+		# page = response.css('td:nth-child(3) font.Category ::text').extract_first()
+		# page = page.replace(u'من','')
+		# page = page.replace(u'صفحة ','')
+		# page = page.replace(u'\u0627\u0644\n\xa01\xa0\n\n\xa0','')
+		# print("***********************************")
+		# print(page)
+		
+		for news in response.css('.More_News_titles'):
+			next_page = news.css('a::attr("href")').extract_first()
 			if next_page:
 				yield scrapy.Request(response.urljoin(next_page),callback=self.news_details)
+		print(href)
 	def news_details(self, response):
+		import sys
+		reload(sys)
+		sys.setdefaultencoding('utf-8')
 		href = response.url
 		details = response.css('.article_content_title ::text').extract_first()
 		article_title = response.css('.article_title ::text').extract_first()
@@ -27,4 +52,4 @@ class BlogSpider(scrapy.Spider):
 		print(details)
 		print(article_title)
 		print(date)
-		yield {"details":details,"href":href,"article_title":article_title,"date":date}
+		yield {"details":details.encode('utf-8'),"href":href,"article_title":article_title,"date":date}
