069933fa1e740de7c2e43e4bad90d87b6c907123
sakthivel-freelancer <sakthivelramanp@gmail.com>
2018-12-08 20:33:58 +0530

    changes

--- news.py
+++ news.py
@@ -7,6 +7,7 @@ from urllib import urlencode
 from scrapy import Request
 from scrapy.http.cookies import CookieJar
 import re
+import urlparse
 class BlogSpider(scrapy.Spider):
 	name = "news"
 	def __init__(self, *args, **kwargs):
@@ -27,16 +28,12 @@ class BlogSpider(scrapy.Spider):
 			page_href = href+"&screen="+str(i)
 			yield scrapy.Request(response.urljoin(page_href),callback=self.category_list)
 	def category_list(self,response):
-		href = response.url
-		# page = response.css('td:nth-child(3) font.Category ::text').extract_first()
-		# page = page.replace(u'من','')
-		# page = page.replace(u'صفحة ','')
-		# page = page.replace(u'\u0627\u0644\n\xa01\xa0\n\n\xa0','')
-		# print("***********************************")
-		# print(page)
-		
+		href = response.url		
 		for news in response.css('.More_News_titles'):
 			next_page = news.css('a::attr("href")').extract_first()
+			par = urlparse.parse_qs(urlparse.urlparse(url).query)
+			news_id = (par['a_id'][0])
+			print(news_id)
 			if next_page:
 				yield scrapy.Request(response.urljoin(next_page),callback=self.news_details)
 		print(href)
@@ -45,11 +42,11 @@ class BlogSpider(scrapy.Spider):
 		reload(sys)
 		sys.setdefaultencoding('utf-8')
 		href = response.url
-		details = response.css('.article_content_title ::text').extract_first()
+		details = response.css('.article_content_title ::text').extract()
 		article_title = response.css('.article_title ::text').extract_first()
 		date = response.css('.Main_article_date ::text').extract_first()
 		
 		print(details)
 		print(article_title)
 		print(date)
-		yield {"details":details.encode('utf-8'),"href":href,"article_title":article_title,"date":date}
+		yield {"details":json.dumps(details,ensure_ascii=False).encode('utf8'),"href":href,"article_title":article_title,"date":date}
